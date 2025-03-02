fca6db352a665f613e861aaa251bf3655806b2f0
_ <_>
2025-03-02 14:52:56 +0900

    Change font size

--- src/App.svelte
+++ src/App.svelte
@@ -22,7 +22,7 @@
 
 <main class="grid h-svh place-items-center bg-neutral-800 p-[3em_0] text-neutral-200">
   <h1 class="p-1 text-3xl font-bold">Quadratic Equation Solver</h1>
-  <div class="p-1 text-3xl">
+  <div class="p-1 text-2xl">
     <input
       bind:value={quadCoef}
       oninput={calc}
@@ -32,7 +32,7 @@
     <input bind:value={constCoef} oninput={calc} class="w-20 rounded-[6px] border-1 border-neutral-500 p-[0_3px] text-right outline-0" />
     = 0
   </div>
-  <div class="p-1 text-3xl">
+  <div class="p-1 text-2xl">
     {#if result === null}
       <span>waiting for input...</span>
     {:else if typeof result === "object"}
